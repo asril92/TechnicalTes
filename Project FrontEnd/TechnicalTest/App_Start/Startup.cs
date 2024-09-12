125223bfa2e7b7d9e2cb094d703754cf9b2c4bbd
asril92 <60920776+asril92@users.noreply.github.com>
2024-09-13 00:38:58 +0700

    Add files via upload

---
+++ Project FrontEnd/TechnicalTest/App_Start/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnicalTest.Startup))]
+namespace TechnicalTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
